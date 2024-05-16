9065a2030a69b7b6fedfedcce4970dd4fced459b
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 18:14:04 +0300

    show single book detail and fetching single book data.

--- src/views/BookDetail.vue
+++ src/views/BookDetail.vue
@@ -1,9 +1,55 @@
 <template>
-  <div>Book Detail</div>
+  <div>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      Book Detail
+      <card-component
+        v-if="book.volumeInfo"
+        :title="book.volumeInfo.title"
+        :description="truncateText(book.volumeInfo.description)"
+        :imageUrl="book.volumeInfo.imageLinks.thumbnail"
+        :cardDetail="true"
+      />
+    </div>
+  </div>
 </template>
+
 <script>
+import { getBookDetails } from "../services/api";
+import CardComponent from "../components/CardComponent.vue";
+
 export default {
   name: "BookDetail",
-  components: {},
+  components: { CardComponent: CardComponent },
+  data() {
+    return {
+      book: {},
+      loading: true,
+      maxTextLength: 100,
+    };
+  },
+  created() {
+    const bookId = this.$route.params.id;
+    this.fetchBookDetails(bookId);
+  },
+  methods: {
+    async fetchBookDetails(bookId) {
+      try {
+        const bookInfo = await getBookDetails(bookId);
+        this.book = bookInfo;
+      } catch (error) {
+        console.error("Error fetching book details:", error);
+      } finally {
+        this.loading = false;
+      }
+    },
+    truncateText(text) {
+      if (text.length <= this.maxTextLength) {
+        return text + " ***";
+      } else {
+        return text.slice(0, this.maxTextLength) + "...";
+      }
+    },
+  },
 };
 </script>
