471c528442f8df786d817d7ac3870b5310c3ee59
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 13:46:59 +0300

    created navbar component.

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>Online Bookstore</h1>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/cart">Shopping Cart</router-link>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: "NavbarComponent",
+  components: {},
+};
+</script>
