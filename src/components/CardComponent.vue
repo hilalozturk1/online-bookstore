422aec20a0137c72bf46a11bdc53da235dbc8a33
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 14:41:46 +0300

    added card component.

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <img :src="imageUrl" alt="Card Image">
+    <div class="card-body">
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+      <button @click="addToCart">Add to Cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComponent",
+  props: {
+    title: String,
+    description: String,
+    imageUrl: String,
+  },
+  methods: {
+    addToCart() {
+      console.log("Add to Cart button clicked");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px;
+  width: 200px;
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+  }
+}
+
+.card img {
+  width: 100%;
+  border-radius: 5px 5px 0 0;
+}
+
+.card-body {
+  text-align: center;
+}
+</style>
+
