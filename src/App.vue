809a95501497b4fc6e4bf61b55668a4486b69f79
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 18:52:17 +0300

    cdn poppins.

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif; /* Poppins fontunu tanımla */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
