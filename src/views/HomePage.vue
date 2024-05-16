96e6c9fac6fd6f0eaceffbaafb818156f37a5b87
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 13:51:23 +0300

    created homepage.

---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><navbar-component /><footer-component /></div>
+</template>
+
+<script>
+import FooterComponent from "../components/FooterComponent.vue";
+import NavbarComponent from "../components/NavbarComponent.vue";
+
+export default {
+  name: "HomePage",
+  components: { NavbarComponent: NavbarComponent, FooterComponent: FooterComponent },
+  data() {
+    return {
+      books: [],
+    };
+  },
+};
+</script>
