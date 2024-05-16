859f6eae91294d66c5ce3c6dbbb59605c691265d
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 18:40:33 +0300

    css navbar.

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -13,3 +13,39 @@ export default {
   components: {},
 };
 </script>
+
+<style scoped>
+header {
+  background-color: #333;
+  padding: 10px;
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
+  margin: 0;
+}
+
+nav {
+  display: flex;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+nav a {
+  color: white;
+  text-decoration: none;
+  margin-left: 10px;
+}
+
+@media only screen and (max-width: 600px) {
+  nav {
+    flex-direction: column;
+  }
+
+  nav a {
+    margin-left: 0;
+    margin-top: 5px;
+  }
+}
+</style>
