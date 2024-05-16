74238268c616146fd1bc7d436244d0b43f27c00d
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-16 17:57:22 +0300

    added get single book.

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,26 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function getBooks(query) {
   try {
     const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
     return response.data.items;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
+    return null;
+  }
+}
+
+async function getBookDetails(bookId) {
+  try {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error:", error);
     return null;
   }
 }
 
 module.exports = {
-  getBooks
+  getBooks,
+  getBookDetails,
 };
