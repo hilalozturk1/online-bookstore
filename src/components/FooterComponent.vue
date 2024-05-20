54943db216847c05e9e90e0596a67cc9ad40c20b
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-20 19:49:14 +0300

    css.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -9,3 +9,13 @@ export default {
   components: {},
 };
 </script>
+<style scoped>
+footer {
+  bottom: 0;
+  width: 100%;
+  background-color: #f8f9fa;
+  text-align: center;
+  padding: 10px 0;
+  box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+}
+</style>
