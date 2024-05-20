c2dfe94e0cb63f486be208e2f719ca6ab35728f0
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-20 19:53:05 +0300

    added bootstrap.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(router).use(store).mount("#app");
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+createApp(App).use(router).use(store).use(BootstrapVue3).mount("#app");
