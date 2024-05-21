c61b53c683d177d0a856d1ec8a22bcbce24c1243
hilalozturk1 <hlloztrk68@gmail.com>
2024-05-21 19:20:39 +0300

    added toast.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import store from "./store";
 
 import "./assets/index.css";
 
+import VueToast from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 import BootstrapVue3 from "bootstrap-vue-3";
 
-createApp(App).use(router).use(store).use(BootstrapVue3).mount("#app");
+createApp(App).use(router).use(store).use(BootstrapVue3).use(VueToast).mount("#app");
